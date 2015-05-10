6950dc3d35dce757ee2ef482818295aaacd9d770
Vance B <vance@axxe.co.uk>
2015-05-10 19:08:48 +0100

    Rewrite of the Summarization class, that allows data to be summarized by a key value, usually expected to be a time period, e.g. minute number, but could be other things

--- summarize.py
+++ summarize.py
@@ -1,87 +1,204 @@
-
 class Summarize():
     def __init__(self):
         self.data = {}
 
-    def logData(self, key, data):
+    def getData(self):
+        return self.data
+
+    def logData(self, key, dataDict):
         if key in self.data:
-            self.data[key].append(data)
+            for field in dataDict:
+                if field in self.data[key]:
+                    self.data[key][field].append(dataDict[field])
+                else:
+                    self.data[key][field] = [dataDict[field]]
         else:
-            self.data[key] = [data]
+            self.data[key] = {}
+            for field in dataDict:
+                self.data[key][field] = [dataDict[field]]
 
-    def delData(self, key):
-        if key in self.data:
-            self.data.pop(key)
+    def max(self, key=None, field=None):
+        result = {}
+        if key == None:
+            for k in self.data:
+                result[k] = {}
+                if field == None:
+                    for f in self.data[k]:
+                        result[k][f] = self._maxArray(k,f)
+                else:
+                    if field in self.data[k]:
+                        result[k][field] = self._maxArray(k,field)
+        else:
+            if key in self.data:
+                result[key] = {}
+                if field == None:
+                    for f in self.data[key]:
+                        result[key][f] = self._maxArray(key,f)
+                else:
+                    if field in self.data[key]:
+                        result[key][field] = self._maxArray(key,field)
+        return result
 
-    def readings(self, key):
+    def min(self, key=None, field=None):
+        result = {}
+        if key == None:
+            for k in self.data:
+                result[k] = {}
+                if field == None:
+                    for f in self.data[k]:
+                        result[k][f] = self._minArray(k,f)
+                else:
+                    if field in self.data[k]:
+                        result[k][field] = self._minArray(k,field)
+        else:
+            if key in self.data:
+                result[key] = {}
+                if field == None:
+                    for f in self.data[key]:
+                        result[key][f] = self._minArray(key,f)
+                else:
+                    if field in self.data[key]:
+                        result[key][field] = self._minArray(key,field)
+        return result
+
+    def avg(self, key=None, field=None):
+        result = {}
+        if key == None:
+            for k in self.data:
+                result[k] = {}
+                if field == None:
+                    for f in self.data[k]:
+                        result[k][f] = self._avgArray(k,f)
+                else:
+                    if field in self.data[k]:
+                        result[k][field] = self._avgArray(k,field)
+        else:
+            if key in self.data:
+                result[key] = {}
+                if field == None:
+                    for f in self.data[key]:
+                        result[key][f] = self._avgArray(key,f)
+                else:
+                    if field in self.data[key]:
+                        result[key][field] = self._avgArray(key,field)
+        return result
+
+    def count(self, key=None, field=None):
+        result = {}
+        if key == None:
+            for k in self.data:
+                result[k] = {}
+                if field == None:
+                    for f in self.data[k]:
+                        result[k][f] = len(self.data(k,f))
+                else:
+                    if field in self.data[k]:
+                        result[k][field] = len(self.data(k,field))
+        else:
+            if key in self.data:
+                result[key] = {}
+                if field == None:
+                    for f in self.data[key]:
+                        result[key][f] = len(self.data[key][f])
+                else:
+                    if field in self.data[key]:
+                        result[key][field] = len(self.data[key][field])
+        return result
+
+    def getNumeric(self, key=None, field=None):
+        result = {}
+        if key == None:
+            for k in self.data:
+                result[k] = {}
+                if field == None:
+                    for f in self.data[k]:
+                        result[k][f] = self._numArray(k,f)
+                else:
+                    if field in self.data[k]:
+                        result[k][field] = self._numArray(k,field)
+        else:
+            if key in self.data:
+                result[key] = {}
+                if field == None:
+                    for f in self.data[key]:
+                        result[key][f] = self._numArray(key,f)
+                else:
+                    if field in self.data[key]:
+                        result[key][field] = self._numArray(key,field)
+        return result
+
+
+    def countNumeric(self, key, field=None):
+        result = {}
+        if field == None:
+            if key in self.data:
+                for field in self.data[key]:
+                    result[field] = len(self.data[key][field])
+        else:
+            result[field] = len(self.data[key][field])
+        return result
+
+
+    def _getNumber(self, value):
+        try:
+            return float(value)
+        except:
+            pass
+        return None
+
+    def _numArray(self, key, field):
+        result = []
         if key in self.data:
-            return len(self.data)
+            if field != None:
+                if field in self.data[key]:
+                    for value in self.data[key][field]:
+                        v = self._getNumber(value)
+                        if v != None:
+                            result.append(v)
+        return result
+
+    def _maxArray(self, key, field):
+        result = None
+        if key in self.data:
+            if field != None:
+                if field in self.data[key]:
+                    for value in self.data[key][field]:
+                        v = self._getNumber(value)
+                        if v != None:
+                            if result == None:
+                                result = v
+                            else:
+                                if v > result:
+                                    result = v
+        return result
+
+    def _minArray(self, key, field):
+        result = None
+        if key in self.data:
+            if field != None:
+                if field in self.data[key]:
+                    for value in self.data[key][field]:
+                        v = self._getNumber(value)
+                        if v != None:
+                            if result == None:
+                                result = v
+                            else:
+                                if v < result:
+                                    result = v
+        return result
+
+    def _avgArray(self, key, field):
+        sum = 0
+        count = 0
+        if key in self.data:
+            if field != None:
+                if field in self.data[key]:
+                    for value in self.data[key][field]:
+                        v = self._getNumber(value)
+                        if v != None:
+                            count += 1
+                            sum += v
+        if count > 0:
+            return sum/count
         else:
             return None
-
-    def max(self, key, field=None):
-        if field:
-            max = None
-            for reading in self.data[key]:
-                if not max:
-                    max = reading[field]
-                else:
-                    if reading[field] > max:
-                        max = reading[field]
-            return max
-        else:
-            max = {}
-            for reading in self.data[key]:
-                if field not in max:
-                    max[field] = reading[field]
-                else:
-                    if reading[field] > max[field]:
-                        max[field] = reading[field]
-            return max
-
-    def min(self, key, field=None):
-        if field:
-            min = None
-            for reading in self.data[key]:
-                if not min:
-                    min = reading[field]
-                else:
-                    if reading[field] < min:
-                        min = reading[field]
-            return min
-        else:
-            min = {}
-            for reading in self.data[key]:
-                if field not in min:
-                    min[field] = reading[field]
-                else:
-                    if reading[field] < min[field]:
-                        min[field] = reading[field]
-            return min
-
-    def avg(self, key, field=None):
-        if field:
-            sum = 0
-            count = 0
-            for reading in self.data[key]:
-                if field in reading:
-                    sum += self.data[key][field]
-                    count += 1
-            if count > 0:
-                return sum/count
-            else:
-                return None
-        else:
-            sum = {}
-            count = {}
-            avg = {}
-            for reading in self.data:
-                for key in reading:
-                    if key in sum:
-                        sum[key] += reading[key]
-                    else:
-                        sum[key] = reading[key]
-                    count[key] += 1
-            for reading in sum:
-                avg[reading] = sum[reading] / count[reading]
-            return avg
